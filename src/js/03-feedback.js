b922f28982359e116da21019f215c20e423b95fd
Ilydanyxx <danya.chuyko@icloud.com>
2023-06-05 12:52:34 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,18 @@
 import throttle from 'lodash.throttle';
 
 const LOCAL_STORAGE = "feedback-form-state";
-const form = document.querySelector(".feedback-form input");
+const form = document.querySelector(".feedback-form");
 let formData = {};
 
-form.addEventListener("input", throttle((e) => {
+form.addEventListener(
+    "input",
+    throttle((e) => {
     formData[e.target.name] = e.target.value.trim();
     localStorage.setItem(LOCAL_STORAGE, JSON.stringify(formData));
-}, 500));
+    }, 500)
+);
 
-window.addEventListener('load', onLoad)
+window.addEventListener("load", onLoad)
 function onLoad() {
     try {
         const data = localStorage.getItem(LOCAL_STORAGE);
@@ -19,7 +22,7 @@ function onLoad() {
             form.element[key].value = val;
         });
     } catch (error) {
-        console.log("error.message");
+        console.log(error.message);
     }
 } 
 
